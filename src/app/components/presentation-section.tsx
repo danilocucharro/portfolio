5a4ab11a3f10e44ddb5eda892830a8749376d90e
danilocucharro <danicucharro@outlook.com>
2026-02-23 17:03:57 -0300

    chore: setting a fixed position on header component

--- src/app/components/presentation-section.tsx
+++ src/app/components/presentation-section.tsx
@@ -4,7 +4,7 @@ import { Button } from "./button";
 export function PresentationSection() {
   return (
     <section
-      className="flex flex-col w-full gap-8 items-center mt-20"
+      className="flex flex-col w-full gap-8 items-center mt-50"
       id="home"
     >
       <div className="flex flex-col max-w-157.5 items-center gap-6">
