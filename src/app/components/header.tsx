fe54db91cc8fca162f11f7da0b65d01ccb645542
danilocucharro <danicucharro@outlook.com>
2026-03-04 15:40:14 -0300

    feat: Adding a modal component

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,23 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
 
 export function Header() {
@@ -42,9 +62,52 @@ export function Header() {
         </nav>
       </header>
 
-      <button className="flex max-md:fixed max-md:bottom-5 max-md:z-50 max-md: px-2 h-15.5 w-40 bg-white text-black-800 shadow-inner items-center justify-center rounded-full cursor-pointer hover:bg-black hover:text-white border border-gray-700 transition-colors duration-200">
-        <p className="underline font-bold text-base">GET IN TOUCH</p>
-      </button>
+      <Dialog>
+        <form>
+          <DialogTrigger className="underline font-bold text-base" asChild>
+            <button className="flex max-md:fixed max-md:bottom-5 max-md:z-50 max-md: px-2 h-15.5 w-40 bg-white text-black-800 shadow-inner items-center justify-center rounded-full cursor-pointer hover:bg-black hover:text-white border border-gray-700 transition-colors duration-200">
+              <p className="underline font-bold text-base">GET IN TOUCH</p>
+            </button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-sm">
+            <DialogHeader>
+              <DialogTitle>Contact me for a project</DialogTitle>
+              <DialogDescription>
+                Give me your contact and ideias here. Click save when
+                you&apos;re done.
+              </DialogDescription>
+            </DialogHeader>
+            <FieldGroup>
+              <Field>
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" name="name" defaultValue="John Doe" />
+              </Field>
+              <Field>
+                <Label htmlFor="username-1">E-mail</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  defaultValue="johndoe@email.com"
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="message">Message</FieldLabel>
+                <FieldDescription>Enter your message below.</FieldDescription>
+                <Textarea
+                  id="message"
+                  placeholder="Type your idea for a project or just contact me."
+                />
+              </Field>
+            </FieldGroup>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Send</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
+      </Dialog>
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Danilo Cucharro",
@@ -32,6 +33,7 @@ export default function RootLayout({
         before:blur-lg"
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
